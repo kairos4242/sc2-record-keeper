9cbbc5183f882dc717e3fcb25a15e607de92cc2c
Riley Ward <riley4242@hotmail.com>
2022-04-15 16:30:35 -0600

    Use Tailwind Movie list framework to start

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav.js'
+import NavItem from './components/NavItem.js'
+import List from './components/List.js'
+import ListItem from './components/ListItem.js'
+
+const movie1 = {
+  title: "Test",
+  starRating: "5",
+  year: 1979,
+  genre: "Action",
+  rating: "18A",
+  runtime: "2h 39m",
+  cast: "Whomst"
+}
+
+const movie2 = {
+  title: "Test",
+  starRating: "5",
+  year: 1979,
+  genre: "Action",
+  rating: "18A",
+  runtime: "2h 39m"
+}
+
+const movies = [movie1, movie2]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p className="text-3xl font-bold underline">
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="divide-y divide-slate-100">
+      <Nav>
+        <NavItem href="/new" isActive>New Releases</NavItem>
+        <NavItem href="/top">Top Rated</NavItem>
+        <NavItem href="/picks">Vincent’s Picks</NavItem>
+      </Nav>
+      <List>
+        {movies.map((movie) => (
+          <ListItem key={movie.id} movie={movie} />
+        ))}
+      </List>
     </div>
-  );
+  )
 }
 
 export default App;

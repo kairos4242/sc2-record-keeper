d098e67b4202f2be71faaa26a0f92a305fa60990
Riley Ward <riley4242@hotmail.com>
2022-04-16 00:56:18 -0600

    TEMPORARILY import legacy data in main function

--- src/components/Race.js
+++ src/components/Race.js
@@ -3,6 +3,7 @@ export default class Race {
     static TERRAN = new Race("terran")
     static PROTOSS = new Race("protoss")
     static ZERG = new Race("zerg")
+    static RANDOM = new Race("random")
     
     constructor(name) {
         this.name = name
